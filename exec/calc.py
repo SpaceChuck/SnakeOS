1ed844b2466bc5b047b367735abecc591cce1239
SpaceChuck <60058320+SpaceChuck@users.noreply.github.com>
2022-01-01 21:34:17 +0300

    Add files via upload

---
+++ exec/calc.py
@@ -0,0 +1,44 @@
+from math import *
+
+
+
+num1 = input("Number 1:")
+try: 
+    num1 = int(num1)
+except Exception:
+    try:
+        num1 = float(num1)
+    except:
+        exit()
+num2 = input("Number 2:")
+try:
+    num2 = int(num2)
+except Exception:
+    try:
+        num2 = float(num2)
+    except Exception:
+        num2 = 0
+op = input("Operation (+,-,*,/,%,**,abs,sqrt,ezp,mog,log10):")
+    
+if op == "+":
+    print("Result: " + str(num1+num2))
+elif op == "-":
+    print("Result: " + str(num1-num2))
+elif op == "*":
+    print("Result: " + str(num1*num2))
+elif op == "/":
+    print("Result: " + str(num1/num2))
+elif op == "%":
+    print("Result: " + str(num1%num2))
+elif op == "**":
+    print("Result: " + str(num1**num2))
+elif op == "abs":
+    print("Result: " + str(abs(num1)))
+elif op == "sqrt":
+    print("Result: " + str(sqrt(num1)))
+elif op == "exp":
+    print("Result: " + str(exp(num1)))
+elif op == "log":
+    print("Result: " + str(log(num1)))
+elif op == "log10":
+    print("Result: " + str(log10(num1)))
---
+++ exec/help.py
@@ -0,0 +1,15 @@
+
+print("""
+
+Built in commands
+
+cd - change directory
+ls - list files
+quit - shutdown SnakeOS
+make - make a file
+makedir - make a directory
+del - delete a directory or a file
+
+ """)
+
+
