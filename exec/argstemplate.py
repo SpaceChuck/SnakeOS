2a7bdc0f569f0d3efb7dd5e92b7c6140789fe07e
SpaceChuck <60058320+SpaceChuck@users.noreply.github.com>
2022-01-08 20:59:38 +0300

    Added argstemplate.py
    A template for using arguments in executables

---
+++ exec/argstemplate.py
@@ -0,0 +1,12 @@
+### SnakeOS Arguments Template
+
+import os
+
+argsfile = open(os.path.join("exec", "args.txt"), "r")
+argsTemp = argsfile.readlines()
+args = []
+for i in argsTemp:
+    if i == argsTemp[len(argsTemp)-1]:
+        args.append(i)
+    else:
+        args.append(i[0:-1])
